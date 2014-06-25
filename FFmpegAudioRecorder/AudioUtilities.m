0d09b4ae7754aaf153d1134ddda996a114728160
albert.liao <alb423@gmail.com>
2014-06-26 01:01:37 +0800

    revise printAACAHeader

--- FFmpegAudioRecorder/AudioUtilities.m
+++ FFmpegAudioRecorder/AudioUtilities.m
@@ -12,6 +12,7 @@
 
 #pragma mark - For specific audio header parser
 
+// Reference http://wiki.multimedia.cx/index.php?title=ADTS
 + (void) printAACAHeader:(tAACADTSHeaderInfo *) pADTSHeader
 {
     //== adts_fixed_header ==
@@ -34,18 +35,38 @@
     //    adts_buffer_fullness; 11 bslbf
     //    number_of_raw_data_blocks_in_frame; 2 uimsfb
     
+    static unsigned const samplingFrequencyTable[16] = {
+        96000, 88200, 64000, 48000,
+        44100, 32000, 24000, 22050,
+        16000, 12000, 11025, 8000,
+        7350, 0, 0, 0
+    };
+    
     printf("== adts_fixed_header ==\n");
     printf("syncword                = 0x%03X\n", pADTSHeader->syncword);
-    printf("ID                      = 0x%02X\n", pADTSHeader->ID);
+    printf("ID                      = 0x%02X ", pADTSHeader->ID);
+    if(pADTSHeader->ID==0) printf("(MPEG-4)");
+    else if(pADTSHeader->ID==1) printf("(MPEG-2)");
+    else printf("(unknown)");
+    printf("\n");
+    
     printf("layer                   = 0x%02X\n", pADTSHeader->layer);
     printf("protection_absent       = 0x%02X\n", pADTSHeader->protection_absent);
-    printf("profile                 = 0x%02X\n", pADTSHeader->profile);
-    printf("sampling_frequency_index= 0x%02X\n", pADTSHeader->sampling_frequency_index);
+    printf("profile                 = 0x%02X ", pADTSHeader->profile);
+    if(pADTSHeader->profile==0) printf("Main profile (AAC MAIN)");
+    else if(pADTSHeader->profile==1) printf("Low Complexity profile (AAC LC)");
+    else if(pADTSHeader->profile==2) printf("Scalable Sample Rate profile (AAC SSR)");
+    else if(pADTSHeader->profile==3) printf("(reserved) AAC LTP");
+    else printf("(unknown)");
+    printf("\n");
+    
+    printf("sampling_frequency_index= 0x%02X (%dHZ)\n", pADTSHeader->sampling_frequency_index, samplingFrequencyTable[pADTSHeader->sampling_frequency_index]);
     printf("private_bit             = 0x%02X\n", pADTSHeader->private_bit);
     printf("channel_configuration   = 0x%02X\n", pADTSHeader->channel_configuration);
     printf("original_copy           = 0x%02X\n", pADTSHeader->original_copy);
     printf("home                    = 0x%02X\n", pADTSHeader->home);
 
+    
     
     printf("== adts_variable_header ==\n");
     printf("copyright_identification_bit        = 0x%02X\n", pADTSHeader->copyright_identification_bit);
